9ad2ab4e12099399e7f75c0ec8506345f20bf804
Ryo Yamamoto <email@ryoyamamoto.jp>
2019-05-22 10:41:18 +0900

    Initial commit

---
+++ notepad.h
@@ -0,0 +1,57 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class Notepad;
+}
+
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Notepad(QWidget *parent = nullptr);
+    ~Notepad();
+
+private slots:
+    void newDocument();
+
+    void open();
+
+    void save();
+
+    void saveAs();
+
+    void print();
+
+    void copy();
+
+    void cut();
+
+    void paste();
+
+    void undo();
+
+    void redo();
+
+    void selectFont();
+
+    void setFontBold(bool bold);
+
+    void setFontItalic(bool italic);
+
+    void setFontUnderline(bool underline);
+
+    void about();
+
+    void exit();
+
+private:
+    Ui::Notepad *ui;
+    QString currentFile;
+};
+
+#endif // NOTEPAD_H
